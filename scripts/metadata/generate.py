d1cd2c95c4d1bdc88b164a6a2194355ced5ac3f8
Teddy Not Bear <notbearteddy@gmail.com>
2022-12-06 14:04:39 +0100

    feat : generate nft & metadata

---
+++ scripts/metadata/generate.py
@@ -0,0 +1,39 @@
+import json
+from datetime import datetime
+import uuid
+
+def generateMetada(dna, id, date):
+    metadata = {
+        "dna": dna,
+        "name": "Garden Collection #" + str(id),
+        "description": "First Garden Collection on Polygon",
+        "image": "ipfs//QmUJrYLXB81sgZmtpvDwXwLiwX7FiNd189CtBDxLmguAjE/" + str(id) + ".jpg",
+        "date": date,
+        "attributes" : [
+            {
+                "trait_value": "DUCK",
+                "value": 3
+            },
+            {
+                "trait_value": "TREE",
+                "value": 2
+            },
+            {
+                "trait_value": "TRASH",
+                "value": "Brown"
+            }
+        ]
+    }
+
+    json_metadata = json.dumps(metadata, indent=4)
+    with open('./scripts/metadata/json/' + str(id) + '.json', 'w') as jsonFile:
+        jsonFile.write(json_metadata)
+        jsonFile.close()
+
+def main():
+    dt = datetime.now()
+    ts = datetime.timestamp(dt)
+    for _ in(i for i in range(10)):
+        generateMetada(uuid.uuid4().hex, _, ts)
+
+main()
