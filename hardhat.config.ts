925e616c262f9caff1b9a2601e9acd496d567269
Teddy Not Bear <notbearteddy@gmail.com>
2022-12-06 13:39:29 +0100

    feat : init hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,7 +1,23 @@
 import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import "dotenv/config";
+
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL || "";
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
+  defaultNetwork: "hardhat",
+  networks: {
+
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY
+  },
+  gasReporter: {
+    enabled: true
+  }
 };
 
 export default config;
